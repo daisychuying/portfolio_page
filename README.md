11dffcf5bf173d394e9ac44c8cf7e82dc0e4ba45
daisychuying <58837611+daisychuying@users.noreply.github.com>
2021-11-27 18:01:50 -0800

    modified javascript, pug templates, and data.json for the app

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # static_node_and_express_site-v1
- Portfolio site
+
+Portfolio site
+In this project, I created a gorgeous portfolio site to showcase the projects I've built.
+
+The site contains a portfolio page to display all my projects, an about page to share my contact info and my skills, and projects pages to show details for each project.
+
+When user hover over the button, the background color will change to light grey.
+I also changed the background color of portfolio page, the font size and color of intro-paragraph in about page, and color of project name in the main page.
---
+++ app.js
@@ -0,0 +1,54 @@
+const express = require('express');
+const { projects } = require('./data.json');
+const path = require('path');
+
+const app = express();
+//view engine setup
+app.set('view engine', 'pug');
+//Add static middleware
+app.use('/static', express.static('public'));
+//set route to home page
+app.get('/', (req, res) => {
+    res.render('index', { projects });
+});
+//set route to about page
+app.get('/about', (req, res) => {
+    res.render('about');
+});
+//set dynamic project route based on project's id
+app.get('/projects/:id', (req, res) => {
+    const { id } = req.params;
+    const projectToShow = projects[id];
+    if (projectToShow){
+        res.render('project', { projectToShow });
+    } else {
+        res.sendStatus(404);
+    }
+})
+//handle 404 error
+app.use((req, res, next) => {
+    const err = new Error('Not Found');
+    err.status = 404;
+    err.message = 'Page Not Found.';
+    console.log(err.status, err.message);
+    //pass error to global error handler
+    next(err);
+})
+//Global error handler
+app.use((err, req, res, next) => {
+
+    if (err.status == 404) {
+        res.status(404);
+        res.render('page-not-found', { err });
+    } else {
+        err.message = err.message || 'Something went wrong on the server.';
+        res.status(err.status || 500);
+        console.log(err.status, err.message)
+        res.render('error', { err });
+    }
+})
+
+//listen on port 3000
+app.listen(process.env.PORT || 3000, () => {
+    console.log('The app is running on localhose:3000');
+})
